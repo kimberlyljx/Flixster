13b4143d5fbe0a63155563cd1c0b5cbd58ffe844
Kimberly Jinxia Lim <klimjinx@klimjinx-mbp.dhcp.thefacebook.com>
2016-06-15 16:04:58 -0700

    Parsed JSON successfully

--- app/src/main/java/com/codepath/flixster/MoviesActivity.java
+++ app/src/main/java/com/codepath/flixster/MoviesActivity.java
@@ -2,11 +2,23 @@ package com.codepath.flixster;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.ListView;
+
+import com.codepath.flixster.models.Movie;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
+import cz.msebera.android.httpclient.Header;
+
 public class MoviesActivity extends AppCompatActivity {
+    ArrayList<Movie> boxMovies;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,21 +26,41 @@ public class MoviesActivity extends AppCompatActivity {
         setContentView(R.layout.activity_movies);
 
         // 1. Get the actual movies
-        ArrayList<Movie> movies = Movie.getFakeMovies();
+        String URL = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(URL, new JsonHttpResponseHandler() {
 
-        // 2. Get the ListView we want to populate
-        ListView lvMovies = (ListView) findViewById(R.id.lvMovies); // must cast
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                JSONArray moviesJSON = null;
+                try {
+                    moviesJSON = response.getJSONArray("results");
 
-        // 3. Create ArrayAdapter (adapter takes the data and maps it to the view)
-        // first param is context, second is layout, third is data
-        // ArrayAdapter<Movie> adapter = new ArrayAdapter<Movie>(this,
-        //        android.R.layout.simple_list_item_1, movies);
-        MoviesAdapter adapter = new MoviesAdapter(this, movies);
+                    boxMovies = Movie.fromJson(moviesJSON);
+                    Log.d("DEBUG", boxMovies.toString());
 
-        // 4. Associate Adapter with the ListView
-        if (lvMovies != null) {
-            lvMovies.setAdapter(adapter);
-        }
+                    // 2. Get the ListView we want to populate
+                    ListView lvMovies = (ListView) findViewById(R.id.lvMovies); // must cast
+
+                    // 3. Create ArrayAdapter (adapter takes the data and maps it to the view)
+                    // ArrayAdapter<Movie> adapter = new ArrayAdapter<Movie>(this,
+                    // android.R.layout.simple_list_item_1, movies);
+                    MoviesAdapter adapter = new MoviesAdapter(getBaseContext(), boxMovies);
+
+                    // 4. Associate Adapter with the ListView
+                    if (lvMovies != null) {
+                        lvMovies.setAdapter(adapter);
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                super.onFailure(statusCode, headers, throwable, errorResponse);
+            }
+        });
+
     }
 
 
