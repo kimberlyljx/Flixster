d691a1e0bd06c0edb81d623d04b1217205b3251f
Kimberly Lim <kimberlyljx@gmail.com>
2016-06-17 07:46:16 -0700

    Rounded images

--- app/src/main/java/com/codepath/flixster/MovieDetailActivity.java
+++ app/src/main/java/com/codepath/flixster/MovieDetailActivity.java
@@ -1,27 +1,39 @@
 package com.codepath.flixster;
 
-import android.content.res.Configuration;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
 import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.JsonHttpResponseHandler;
 import com.squareup.picasso.Picasso;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
-import cz.msebera.android.httpclient.Header;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import cz.msebera.android.httpclient.entity.mime.Header;
+import jp.wasabeef.picasso.transformations.RoundedCornersTransformation;
 
-class MovieDetailActivity extends YouTubeBaseActivity {
+public class MovieDetailActivity extends YouTubeBaseActivity {
+    @BindView(R.id.tvMovieDetailTitle) TextView tvTitle;
+    @BindView(R.id.tvMovieDetailOverview) TextView tvOverview;
+    @BindView(R.id.rbMovieDetailRating) RatingBar rbRating;
+    @BindView(R.id.ivMovieDetailPic) ImageView ivMovieDetailPic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_detail);
+        ButterKnife.bind(this);
 
         // Get the data
         String movieTitle = getIntent().getStringExtra("title");
@@ -30,59 +42,59 @@ class MovieDetailActivity extends YouTubeBaseActivity {
         Double vote_average = getIntent().getDoubleExtra("vote_average", 0.00); // 0.00 default
         int id = getIntent().getExtras().getInt("id");
 
-        // Q: How to findViewById in specific subtree?
-        TextView tvTitle = (TextView) findViewById(R.id.tvMovieDetailTitle);
-        TextView tvOverview = (TextView) findViewById(R.id.tvMovieDetailOverview);
-        RatingBar rbRating = (RatingBar) findViewById(R.id.rbMovieDetailRating);
-        ImageView ivPoster = (ImageView) findViewById(R.id.ivMovieDetailPic);
+        String imageUri;
+        // Loading a remote image thru URL
+        imageUri = "https://image.tmdb.org/t/p/w342" + backdropPath;
+        Picasso.with(this).load(imageUri)
+                .placeholder(R.drawable.movie_placeholder)
+                .transform(new RoundedCornersTransformation(15, 15))
+                .into(ivMovieDetailPic);
 
         tvTitle.setText(movieTitle);
         tvOverview.setText(movieOverview);
         float rating = vote_average.floatValue();
         rbRating.setRating(rating) ;
 
-
         // 2. Get the actual movies
         String URL = "http://api.themoviedb.org/3/movie/" + id + "/videos" + "?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
         AsyncHttpClient client = new AsyncHttpClient();
         client.get(URL, new JsonHttpResponseHandler() {
-            @Override
+
             public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
                 JSONArray movieVideosJSON = null;
-//                try {
-//                    movieVideosJSON = response.getJSONArray("results");
-//                    JSONObject movieVideo = movieVideosJSON.getJSONObject(0);
-//
-//
-//                    Log.d("DEBUG", movieVideos.toString());
-//
-//                    // 3. Create ArrayAdapter (adapter takes the data and maps it to the view)
-//                    adapter = new MoviesAdapter(getBaseContext(), boxMovies);
-//
-//                    // 4. Associate Adapter with the ListView
-//                    if (lvMovies != null) {
-//                        lvMovies.setAdapter(adapter);
-//                    }
-//                } catch (JSONException e) {
-//                    e.printStackTrace();
-//                }
+                try {
+                    movieVideosJSON = response.getJSONArray("results");
+                    final JSONObject movieVideo = movieVideosJSON.getJSONObject(0);
+                    Log.d("DEBUG", movieVideo.toString());
+                    YouTubePlayerView youTubePlayerView = (YouTubePlayerView) findViewById(R.id.player);
+
+                    youTubePlayerView.initialize("AIzaSyDoJBDU7XVpoLPK8J_NaPlj1ZwAzmu363E",
+                    new YouTubePlayer.OnInitializedListener() {
+                    @Override
+                    public void onInitializationSuccess(YouTubePlayer.Provider provider,
+                                                        YouTubePlayer youTubePlayer, boolean b) {
+
+                        // do any work here to cue video, play video, etc.
+                        try {
+                            youTubePlayer.cueVideo(movieVideo.getString("key"));
+                            Log.d("DEBUG", movieVideo.getString("key").toString());
+                        } catch (JSONException e) {
+                            Log.d("DEBUG", "No movie string");
+                        }
+                    }
+                    @Override
+                    public void onInitializationFailure(YouTubePlayer.Provider provider,
+                                                        YouTubeInitializationResult youTubeInitializationResult) {
+                        Log.d("DEBUG", "Failed to initialize");
+                    }
+                });
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
             }
-            @Override
-            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
-                super.onFailure(statusCode, headers, throwable, errorResponse);
+            public void onFailure(Throwable e) {
+                Log.d("DEBUG", "Player error: " + e.toString());
             }
         });
-
-        String imageUri;
-        // Loading a remote image thru URL
-        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
-            imageUri = "https://image.tmdb.org/t/p/w342" + backdropPath;
-        } else {
-            imageUri = "https://image.tmdb.org/t/p/w342" + backdropPath;
-        }
-
-        Picasso.with(this).load(imageUri)
-                .placeholder(R.drawable.movie_placeholder)
-                .into(ivPoster);
     }
 }
